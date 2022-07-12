11fc69e32fad7008b474a442492435d987beb7f7
Roberto Polo <roberto.polo@outlook.com>
2022-07-12 14:08:49 -0500

    cambios palindromo

--- palindromo.py
+++ palindromo.py
@@ -2,11 +2,20 @@
 # palabra_frase = palabra_frase.lower().replace(' ', '')
 # print(palabra_frase)
 
-def palindromo():
-    pass
+def palindromo(palabra_frase):
+    palabra_frase_invertida = palabra_frase[::-1]
+    if palabra_frase == palabra_frase_invertida:
+        return True
+    else:
+        return False
 
 def run():
-    pass
+    texto_ingresado = input('Ingresa la palabra o frase a comprobar: ').lower().replace(' ', '')
+    es_palindromo = palindromo(texto_ingresado)
+    if es_palindromo:
+        print('Es palindromo')
+    else:
+        print('No es palindromo')
 
 if __name__ == '__main__':
     run()
