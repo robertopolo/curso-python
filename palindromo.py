3d47d23618f0e725cf591f88dfb31e1ab1782403
Roberto Polo <roberto.polo@outlook.com>
2022-07-12 12:13:57 -0500

    inicio palindromo

---
+++ palindromo.py
@@ -0,0 +1,12 @@
+# palabra_frase = input('Ingrese la palabra o frase a comprobar: ')
+# palabra_frase = palabra_frase.lower().replace(' ', '')
+# print(palabra_frase)
+
+def palindromo():
+    pass
+
+def run():
+    pass
+
+if __name__ == '__main__':
+    run()
