2672e5aaea4d8d052297f83550094db887c1ad3b
Roberto Polo <roberto.polo@outlook.com>
2022-07-16 14:06:35 -0500

    ajustes

--- adivina-el-numero.py
+++ adivina-el-numero.py
@@ -2,8 +2,18 @@ import random
 
 
 def run():
+    z = True
+    while z:
+        y = input('Adivina el número (del 1 a 100): ')
+        try:
+            y = int(y)
+        except:
+            print('\n¡ERROR!\nEl programa solo admite números enteros.\nIntente de nuevo.\n')
+        else:
+            z = False
+
+
     x = random.randint(1, 100)
-    y = int(input('Adivina el número (del 1 a 100): '))
     while y != x:
         if y < x:
             print('Elige un número mayor.')
@@ -11,7 +21,6 @@ def run():
             print('Elige un número menor.')
         y = int(input('Elige otro número (del 1 a 100): '))
     print('¡Ganaste!')
-
 
 
 if __name__ == '__main__':
---
+++ test.py
@@ -0,0 +1,9 @@
+x = 0
+z = True
+
+while z:
+    if x < 100:
+        x += 1
+        print(x)
+    else:
+        z = False
