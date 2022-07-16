b442af5d99dedfbc43163cd2cc6cdb0ebf8fc75b
Roberto Polo <roberto.polo@outlook.com>
2022-07-16 10:00:32 -0500

    juego adivina el número

---
+++ adivina-el-numero.py
@@ -0,0 +1,19 @@
+import random
+
+
+def run():
+    x = random.randint(1, 100)
+    y = int(input('Adivina el número (del 1 a 100): '))
+    while y != x:
+        if y < x:
+            print('Elige un número mayor.')
+            y = int(input('Elige otro número (del 1 a 100): '))
+        else:
+            print('Elige un número menor.')
+            y = int(input('Elige otro número (del 1 a 100): '))
+    print('¡Ganaste!')
+
+
+
+if __name__ == '__main__':
+    run()
